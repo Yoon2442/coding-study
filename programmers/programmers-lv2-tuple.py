ed3a87be55cfb649a4a1837bb25a2fb94fd0503d
Yoon2442 <yoonhw20@naver.com>
2023-04-20 10:43:45 +0900

    test: Coding study - programmers

---
+++ programmers/programmers-lv2-tuple.py
@@ -0,0 +1,14 @@
+# Programmers - 2019 카카오 개발자 겨울 인턴십 (튜플)
+def solution(s):
+    s=s.replace(",{","")
+    s=s[2:len(s)-2]
+    s=s.split("}")
+    for i in range(len(s)):
+        s[i]=s[i].split(",")
+        s[i]=[int(j) for j in s[i]]
+    s.sort(key=len)
+    result=[s[0]]
+    for i in range(1,len(s)):
+        result.append([x for x in s[i] if x not in s[i-1]])
+    result=[result[i][0] for i in range(len(result))]
+    return result
